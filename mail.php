dc1824f052871d5eb1769bdd073b82d421828d69
kim-73 <kimlyndonhowell@gmail.com>
2016-05-28 09:24:20 +1000

    contact us form test

--- mail.php
+++ mail.php
@@ -1,4 +1,10 @@
 <?php
 // Where will you get the forms' results?
-define("CONTACT_FORM", 'kimlyndonhowell@gmail.com');
+// define("CONTACT_FORM", 'kimlyndonhowell@gmail.com');
+$postData = $_POST;
+echo $postData["fullname"];
+echo $postData["email"];
+echo $postData["subject"];
+echo $postData["message"];
+//mail("kimlyndonhowell@gmail.com","My subject",$msg);
 ?>
